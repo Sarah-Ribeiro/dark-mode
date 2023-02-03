4408f52051966b168243aa3885985eed431dbe56
Sarah-Ribeiro <cogolsarah0912005@gmail.com>
2023-02-03 15:47:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function dark() {
+    document.querySelector('body').style.backgroundColor = 'black'
+}
+
+function light() {
+    document.querySelector('body').style.backgroundColor = 'white'
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+<h1 align="center">Dark Mode</h1>
+
+<p align="center">This is a dark mode website</p>
+
+## 🖥️ Tecnologias
+
+- HTML
+- CSS
+- Javascript
+- Git
